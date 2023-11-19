20b8c88778c9e2640f75fb30c837af08692eddbb
Luis <101330821+luiis02@users.noreply.github.com>
2023-11-19 20:26:27 +0100

    web

---
+++ js/escribir.js
@@ -0,0 +1,39 @@
+function escribirEnPantalla(texto) {
+  let arr = texto.split("");
+  let i = 0;
+  let mostrarBarra = true;
+
+  function actualizarPantalla() {
+    let intervalo = setInterval(function () {
+      if (i == arr.length) {
+
+        clearInterval(intervalo);
+        setTimeout(() => {
+          document.getElementById("titulo1").innerHTML = "";
+          i = 0;
+          mostrarBarra = true;
+          actualizarPantalla();
+        }, 1000); // Pausa de 1 segundo antes de reiniciar
+      } else {
+        if (arr[i] == " ") {
+          document.getElementById("titulo1").innerHTML += arr[i];
+          document.getElementById("titulo1").innerHTML += arr[i + 1];
+          i += 2;
+        } else {
+          document.getElementById("titulo1").innerHTML += arr[i];
+          i++;
+        }
+      }
+
+    }, 500);
+  }
+
+  actualizarPantalla();
+}
+
+
+
+
+// Llamada a la función
+escribirEnPantalla('cout << "Hey recruiters";');
+
